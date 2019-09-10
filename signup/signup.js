edb9f7fb716974ca2a0d7ea28974978070a1097f
Lúcio Reis <lucio@midiacom.uff.br>
2019-09-10 08:39:40 -0300

    rest&rest v 1.0 09 sept

---
+++ signup/signup.js
@@ -0,0 +1,9 @@
+function signUpAttempt(){
+    console.log(window.location.href);
+    window.location.href = "../index/index.html";
+}
+
+function backToLoggin(){
+    console.log(window.location.href);
+    window.location.href = "../login/login.html";
+}
